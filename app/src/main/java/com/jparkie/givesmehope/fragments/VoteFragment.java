90efa604cf8536a5f0c17d20864389a50adab75c
jparkie <park.jacob.96@gmail.com>
2014-11-01 20:31:02 -0400

    Added Jsoup as Gradle dependency instead of a .Jar.

--- app/src/main/java/com/jparkie/givesmehope/fragments/VoteFragment.java
+++ app/src/main/java/com/jparkie/givesmehope/fragments/VoteFragment.java
@@ -1,6 +1,5 @@
 package com.jparkie.givesmehope.fragments;
 
-import android.app.Activity;
 import android.graphics.PorterDuff;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
